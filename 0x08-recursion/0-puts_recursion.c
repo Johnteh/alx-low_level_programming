621995b17d7e7efd38b9e0df370d418d6894f0d7
root <you@example.com>
2022-03-31 02:41:15 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+include "main.h"
+/**
+ * _puts_recursion: calls a copy of itself
+ * @s: string to print
+ *
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+	 
+	_putchar(s[i]);
+	i++;
+
+	if (i=n);
+		return s;	
+	else;
+	_puts_recursion(s[i]);
+
+}
