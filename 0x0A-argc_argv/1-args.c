53d0dfdbf6226a70b2b86cf95df935091e93afc7
root <you@example.com>
2022-04-04 06:56:31 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function 
+ * @argc: conts arguments
+ * @argv: holds the arguments
+ *Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	if (argc > o)
+		printf("%d\n", argc-1);
+	return (0);
+}
