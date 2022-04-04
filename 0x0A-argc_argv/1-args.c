db4786241b9cd5e2b1c450e418134b331faf9e78
root <you@example.com>
2022-04-04 06:59:23 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: holds the arguments
  *Return: 0
  */
-int main (int argc, char *argv[])
+int main (int argc, char *argv[] __attribute__((unused)))
 {
 	if (argc > o)
 		printf("%d\n", argc-1);
