259d514381e8863c56d6fc0857c9be8d7952599a
root <you@example.com>
2022-04-04 07:02:02 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - function 
- * @argc: conts arguments
- * @argv: holds the arguments
- *Return: 0
+ * main - prints its name, followed by a new line
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
  */
-int main (int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	if (argc > o)
-		printf("%d\n", argc-1);
+	if (argc > 0)
+		printf("%d\n", argc - 1);
 	return (0);
 }
