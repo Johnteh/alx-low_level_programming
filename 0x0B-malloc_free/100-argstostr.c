f4aa6d7891598debf46629b2215a4ba65a4e347f
root <you@example.com>
2022-04-06 05:42:09 -0700

    malloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,12 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * argstostr - prints args
- * @ac: takes in width of grid
- * @av: height of grid
- * Return: the args one line at a time
- */
 
 char *argstostr(int ac, char **av)
 {
