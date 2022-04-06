66a498a0c011d0322e7f714dd1638a23a1cb7465
root <you@example.com>
2022-04-06 05:44:05 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * argstostr - prints args
+ * @ac: takes in width of grid
+ * @av: height of grid
+ * Return: the args one line at a time
+ */
 char *argstostr(int ac, char **av)
 {
 	char *str;
