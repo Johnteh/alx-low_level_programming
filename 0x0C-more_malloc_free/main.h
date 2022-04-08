f10ea28bfc51ae3a51e85f379dc6c466e06877a0
root <you@example.com>
2022-04-08 05:54:39 -0700

    more on mallocs

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,5 +11,16 @@ int _strlen(char *s)
 
 void *_calloc(unsigned int nmemb, unsigned int size);
 
+int _putchar(char c);
+
+int *array_range(int min, int max);
+
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+
+
 
 #endif
