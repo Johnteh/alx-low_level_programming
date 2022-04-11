b81d5754a06f09e7aefce1fdb15bc2d923810179
root <you@example.com>
2022-04-10 21:57:17 -0700

    malloc

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,24 +1,40 @@
 #ifndef FILE_HOLBERTON
 #define FILE_HOLBERTON
 
-#include <stdio.h>
-#include <stdlib.h>
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
 
-char *_strdup(char *str);
+int _putchar(char c);
 
-char **strtow(char *str);
-
-char *argstostr(int ac, char **av);
-
-char *str_concat(char *s1, char *s2);
-
-void free_grid(int **grid, int height);
-
-int **alloc_grid(int width, int height);
+/**
+ * create_array - creates array of chars and initialize it with a specific char
+ * @size: array size
+ * @c: fill array values with this char
+ * Return: pointer to array
+ */
 
 char *create_array(unsigned int size, char c);
 
-char **strtow(char *str);
+/**
+ * _strdup - duplicates a string
+ * @str: duplicate this string
+ * Return: duplicated string
+ */
 
-int _putchar(char c);
+char *_strdup(char *str);
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string one to concatenate
+ * @s2: string two to concatenate
+ * Return: string one and two concatenated
+ */
+
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 #endif
